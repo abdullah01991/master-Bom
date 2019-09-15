4ca5b78cf483b475039a7df44eacd86c64859380
JacobLFulton <54561215+JacobLFulton@users.noreply.github.com>
2019-09-14 20:00:14 -0500

    Update scanner_releases_gantt.php

--- scanner_releases_gantt.php
+++ scanner_releases_gantt.php
@@ -8,6 +8,9 @@
   global $db;
 
   ?>
+<!DOCTYPE HTML>
+<html>
+<head>
 
 
 <div class="right-content">
@@ -16,16 +19,67 @@
       <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
          <h3><img src="images/releases_gantt.png" style="max-height: 35px;" />System Releases Gantt</h3>
          <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    
+         <script type="text/javascript">
+    google.charts.load('current', {'packages':['gantt']});
+    google.charts.setOnLoadCallback(drawChart);
+ function drawChart() {
         
         TODO: Gantt Chart Code
+        <?php
 
-        
+$sql = "SELECT * from releases ORDER BY id ASC, ORDER BY open_date ASC, ORDER BY rtm_date ASC;";
+$result = $db->query($sql);       
 
- <style>
-   tfoot {
-     display: table-header-group;
-   }
- </style>
+                if ($result->num_rows > 0) {
+                  // output data of each row
+                 while($row = $result->fetch_assoc()) {
+                     echo '<tr>
+                              <td>'.$row["id"].'</td>
+                              <td>'.$row["name"].' </span> </td>
+                              <td>'.$row["type"].'</td>
+                              <td>'.$row["status"].'</td>
+                              <td>'.$row["open_date"].' </span> </td>
+                              <td>'.$row["dependency_date"].'</td>
+                              <td>'.$row["freeze_date"].'</td>
+                              <td>'.$row["rtm_date"].' </span> </td>
+                              <td>'.$row["manager"].' </span> </td>
+                              <td>'.$row["author"].' </span> </td>
+                              <td>'.$row["app_id"].' </span> </td>
+                          </tr>';
+                  }//end while
+              }//end if
+              else {
+                  echo "0 results";
+                }//end else
+
+                  $result->close();
+                ?>
+ var data = google.visualization.arrayToDataTable([
+
+ ]);
+
+  var options = {
+        height: 400,
+        gantt: {
+          trackHeight: 30
+        }
+      };
+
+  var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+ chart.draw(data,options);
+ }
+  </script>
+
+
 
   <?php include("./footer.php"); ?>
+
+  
+</head>
+<body>
+ <div class="container-fluid">
+ <div id="chart_div"></div>
+ </div>
+ 
+</body>
+</html>
