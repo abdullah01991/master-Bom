ca7fb985737fc73ccdd0d2e9f8c73a01c9f4ae97
JacobLFulton <54561215+JacobLFulton@users.noreply.github.com>
2019-09-16 16:49:10 -0500

    Update scanner_releases_gantt.php

--- scanner_releases_gantt.php
+++ scanner_releases_gantt.php
@@ -15,7 +15,7 @@
 
       <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
 
-        <h3><img src="images/gantt.png" style="max-height: 35px;" />System Releases Gantt</h3>
+        <h3><img src="images/gantt.png" style="max-height: 35px;" />System Releases</h3>
 
 
 
@@ -41,34 +41,33 @@
       data.addColumn('string', 'Dependencies');
 
       data.addRows([
-        ['2014Spring', 'Spring 2014', 'spring',
-         new Date(2014, 2, 22), new Date(2014, 5, 20), null, 100, null],
-        ['2014Summer', 'Summer 2014', 'summer',
-         new Date(2014, 5, 21), new Date(2014, 8, 20), null, 100, null],
-        ['2014Autumn', 'Autumn 2014', 'autumn',
-         new Date(2014, 8, 21), new Date(2014, 11, 20), null, 100, null],
-        ['2014Winter', 'Winter 2014', 'winter',
-         new Date(2014, 11, 21), new Date(2015, 2, 21), null, 100, null],
-        ['2015Spring', 'Spring 2015', 'spring',
-         new Date(2015, 2, 22), new Date(2015, 5, 20), null, 50, null],
-        ['2015Summer', 'Summer 2015', 'summer',
-         new Date(2015, 5, 21), new Date(2015, 8, 20), null, 0, null],
-        ['2015Autumn', 'Autumn 2015', 'autumn',
-         new Date(2015, 8, 21), new Date(2015, 11, 20), null, 0, null],
-        ['2015Winter', 'Winter 2015', 'winter',
-         new Date(2015, 11, 21), new Date(2016, 2, 21), null, 0, null],
-        ['Football', 'Football Season', 'sports',
-         new Date(2014, 8, 4), new Date(2015, 1, 1), null, 100, null],
-        ['Baseball', 'Baseball Season', 'sports',
-         new Date(2015, 2, 31), new Date(2015, 9, 20), null, 14, null],
-        ['Basketball', 'Basketball Season', 'sports',
-         new Date(2014, 9, 28), new Date(2015, 5, 20), null, 86, null],
-        ['Hockey', 'Hockey Season', 'sports',
-         new Date(2014, 9, 8), new Date(2015, 5, 21), null, 89, null]
+
+         <?php
+
+$sql = "SELECT id,name,type,open_date,rtm_date,UNIX_TIMESTAMP(rtm_date)-UNIX_TIMESTAMP(open_date) AS duration from releases ORDER BY open_date ASC;";
+$result = $db->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo '["'.$row["id"].'","'
+                                 .$row["name"].'","'
+                                 .$row["type"].'", new Date('
+                                 .$row["open_date"].'), new Date('
+                                 .$row["rtm_date"].'),'
+                                 .$row["duration"].',null,null],';
+                    }//end while
+                }//end if
+                else {
+                    echo "0 results";
+                }//end else
+
+                 $result->close();
+                ?>
       ]);
 
       var options = {
-        height: 400,
+        height:800,
         gantt: {
           trackHeight: 30
         }
