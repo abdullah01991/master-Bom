77996fee580686d1176f00329d8000273f9ba795
abdullah01991 <46853738+abdullah01991@users.noreply.github.com>
2019-11-04 16:54:30 -0600

    Create left_menu_reports.php

---
+++ left_menu_reports.php
@@ -0,0 +1,9 @@
+<div id="menu-left">
+
+<a href="reports_bom_report.php">
+  	<div <?php if($left_selected == "REPORT")
+  	{ echo 'class="menu-left-current-page"'; } ?>>
+  	<img src="./images/reports_pie.png">
+  	<br/>BOM Reports<br/></div>
+  </a>
+</div>
