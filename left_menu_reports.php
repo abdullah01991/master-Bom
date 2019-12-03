3e57ca680d79b56125bd27dbf1af6806199aae35
abdullah01991 <46853738+abdullah01991@users.noreply.github.com>
2019-12-02 19:03:41 -0600

    Update left_menu_reports.php

--- left_menu_reports.php
+++ left_menu_reports.php
@@ -4,6 +4,12 @@
   	<div <?php if($left_selected == "REPORT")
   	{ echo 'class="menu-left-current-page"'; } ?>>
   	<img src="./images/reports_pie.png">
-  	<br/>BOM Reports<br/></div>
+  	<br/>Pie Chart<br/></div>
+</a>
+<a href="reports_bom_report_barChart.php">
+  	<div <?php if($left_selected == "REPORT")
+  	{ echo 'class="menu-left-current-page"'; } ?>>
+  	<img src="./images/reports_bar.png">
+  	<br/>Bar Chart<br/></div>
   </a>
 </div>
